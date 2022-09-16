f70474cf64b36e51bd4cb91f86f0cdb3fbc126d6
JenLynX <104281812+JenLynX@users.noreply.github.com>
2022-09-16 11:38:22 -0700

    added footer with github link

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      {" "}
+      Coded by Jen Xamin, open source on{" "}
+      <a
+        href="https://github.com/JenLynX/weather-search-react"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {" "}
+        GitHub{" "}
+      </a>{" "}
+    </footer>
+  );
+}
